b0ce28e7785bdbf75be375ca99a06d216934f450
im-ashar <muhammadashar.401@gmail.com>
2023-09-14 11:23:24 +0500

    Add project files.

---
+++ RedDrop/wwwroot/tailwind.config.js
@@ -0,0 +1,27 @@
+﻿/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        '../**/*.{razor,html,cshtml}',
+        './**/*.js',
+        "./node_modules/tw-elements/dist/js/**/*.js",
+    ],
+    plugins: [
+        require("tw-elements/dist/plugin.cjs"),
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/typography'),
+        require('kutty'),
+    ],
+    theme: {
+        fontFamily: {
+            sans: ["Roboto", "sans-serif"],
+            body: ["Roboto", "sans-serif"],
+            mono: ["ui-monospace", "monospace"],
+        },
+        colors: {
+            "main": "#CC1B1B",
+            "white":"FFFFFF",
+        }
+    },
+    darkMode: "class",
+};
